1201e3278971290534e2c3a1c1642dcbfde0a5e6
hortencia718 <hortenciaecisneros@gmail.com>
2021-03-25 12:39:03 -0400

    email

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // $(document).ready();{
 //     console.log()
 // }
+
+// window.onload = function(){
+//     document.getElementById('contact-form').addEventListener('submit', function(event){
+//         event.preventDefault();
+//         // generate a five digit number for the contact_number varible
+//         this.contact_number.value = Math.random() * 100000 | 0;
+//         // id's from pervious steps 
+//         emailjs.sendform('contact_service', 'contact_form', this)
+//         .then(function(){
+//             console.log('FAILED...', error);
+//         });
+//     });
+// }
