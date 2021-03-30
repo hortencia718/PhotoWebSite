688ba6a5a9ea26b93d564414d6771c2ee71f3ffd
hortencia718 <hortenciaecisneros@gmail.com>
2021-03-30 07:55:36 -0400

    index change to home

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-// //
-
-// const nodemailer = require('nodemailer');
-// const { getMaxListeners } = require('process');
-
-// let mailTransporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: 'xyz@gmail.com',
-//         pass: '*************'
-//     }
-// });
-
-//  let mailDetails = {
-//      from: 'xyz@gmail.com',
-//      to: 'abc@gmail.com',
-//      subject: 'test mail',
-//      text: 'Node.js testing mail for GeeksforGeeks'
-//  };
-
-//  mailTransporter.sendMail (mailDetails, function(err,data){
-//         if(err){
-//            console.log('Error Occurs')
-//         } else {
-//             console.log('Email sent successfully');
-//         }
-//  });
-
+function sendEmail() {
+    sendEmail.send({
+        Host : "smtp.mailtrap.io",
+        Username : "<Mailtrap username>",
+        Password : "<Mailtrap password>",
+        To : 'recipient@example.com',
+        From : "sender@example.com",
+        Subject : "Test email",
+        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+    }).then(
+      message => alert(message)
+    );
+    
+  }
 
 
 
