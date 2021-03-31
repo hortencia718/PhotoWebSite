c8bc6e45837a300338c8bdeb96449f6201c4a99a
hortencia718 <hortenciaecisneros@gmail.com>
2021-03-31 13:08:33 -0400

    public on img photo

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-function sendEmail() {
-    sendEmail.send({
-        Host : "smtp.mailtrap.io",
-        Username : "<Mailtrap username>",
-        Password : "<Mailtrap password>",
-        To : 'recipient@example.com',
-        From : "sender@example.com",
-        Subject : "Test email",
-        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
-    }).then(
-      message => alert(message)
-    );
+
+
+
+
+// function sendEmail() {
+//     sendEmail.send({
+//         Host : "smtp.mailtrap.io",
+//         Username : "<Mailtrap username>",
+//         Password : "<Mailtrap password>",
+//         To : 'recipient@example.com',
+//         From : "sender@example.com",
+//         Subject : "Test email",
+//         Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+//     }).then(
+//       message => alert(message)
+//     );
     
-  }
+//   }
 
 
 
