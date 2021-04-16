83c6e038f3debf8097bafa5adf885a35feb5e04a
hortencia718 <hortenciaecisneros@gmail.com>
2021-04-16 14:48:13 -0400

    added changes

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
-
+require("dotenv").config();
 
 // get the form by its id , stable elements here 
-const form = document.querySelector("#contact-form");
-console.log(form)
-const name= document.querySelector("#name");
-console.log (name)
+const myForm = document.querySelector("#contact-form");
+console.log(myForm)
+ const name= document.querySelector("#name");
+// // console.log (name)
 
-const email= document.querySelector("#email");
-console.log (Email)
+ const email= document.querySelector("#email");
+// console.log (Email)
+const message=document.querySelector('message')
 
-// const submit= document.querySelector("#message");
-// console.log (submit)
 
 
 // add event listener, its the input of submit, no button
 // const formEvent= 
-form.addEventListener("submit", (event) => {
+// form, submit, evt.preventDefault
+// should be going to form, submit, value
 
-    console.log("just submitted the message")
-    event.preventDefault();
-    // debugger
-    // let mail = new FormData(form)
+myForm.addEventListener("submit", (evt) => {
+// console.log("hello i submitted")
+    evt.preventDefault();
+    console.log(process.env.FORM_SPREE_API_URL )
+
+    let email = new FormData(myForm)
+    fetch("https://formspree.io/f/mqkwzgoy",{
+           method: "POST",
+           headers:{
+               'Content-Type': 'application/json',
+               'Response': 'application/json'
+           },
+           body: email
+    })
+          .then(res => res.json())
+         .then((formObj) => {
+            console.log(formObj)
+        })
+
+    })
+
+    
+
+
+// let email = new FormData(form)
+// forma data object(form from line 4)
+// a collection of key/value paris, matches the name and its value & email
     
 
 //   const  SendMessage= (mail) =>{
     //   fetch("https://formspree.io/f/mqkwzgoy"){
         //   method: "POST",
-        //   body: mail
+        //   body: formData
 
-    //   }.then((response)=>{
-        //   return response.json();
-    //   });
+    //   }).then(function (response)=>{
+        //   return response.json(text)();
+    //   }).then function (text){
+        // console.log(text)
+    // }
 //   };
-})
+// })
    
    
    
@@ -67,3 +92,15 @@ form.addEventListener("submit", (event) => {
 
 
 
+// delegation
+// find a stable element
+// add an event listener
+// add an if statment, checking to see what the children have in common
+// do whatever you want evt.target
+
+// form.addEventListener("submit", function(evt){
+    // if(evt.target.tagName === "id"){
+// console.log("you submitted an id")
+    // }
+//  console.log(evt.target)
+// })
---
+++ server.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const app = express();
+// const fetch = require('node-fetch')
+
+// const PORT = process.env.PORT || 5000;
+
+// app.get('/', (req, res) => {
+//     res.send('hello!!')
+// })
+
+// app.listen(PORT, ()=> {
+//     console.log('server running on port ${PORT}')
+// })
